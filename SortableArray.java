4358d5d585b3b66353f2e4081d97e147a7ce2ae8
khizzergit <khizzer.aziz@student.kpu.ca>
2024-11-18 23:18:00 -0800

    string sorting

--- SortableArray.java
+++ SortableArray.java
@@ -11,6 +11,17 @@ public class SortableArray {
             System.out.print(num + " ");
         }
         System.out.println();
+
+        // String sorting example
+        String[] strings = {"apple", "orange", "banana", "pear"};
+        bubbleSortStrings(strings);
+
+        // Print sorted strings
+        System.out.println("Sorted Strings:");
+        for (String str : strings) {
+            System.out.print(str + " ");
+        }
+        System.out.println();
     }
 
     // Bubble Sort method for integers
@@ -19,8 +30,21 @@ public class SortableArray {
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (array[j] > array[j + 1]) {
-                    // Swap array[j] and array[j+1]
                     int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // Bubble Sort method for strings
+    public static void bubbleSortStrings(String[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j].compareTo(array[j + 1]) > 0) {
+                    String temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
                 }
