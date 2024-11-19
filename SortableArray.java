339a5453605e0ead7a8b9d1b5e98f44616d968d5
khizzergit <khizzer.aziz@student.kpu.ca>
2024-11-18 23:11:31 -0800

    integer sort

--- SortableArray.java
+++ SortableArray.java
@@ -1,6 +1,30 @@
 public class SortableArray {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the SortableArray project!");
+        // Integer sorting example
+        int[] integers = {5, 2, 9, 1, 5, 6};
+        bubbleSortIntegers(integers);
+
+        // Print sorted integers
+        System.out.println("Sorted Integers:");
+        for (int num : integers) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    // Bubble Sort method for integers
+    public static void bubbleSortIntegers(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    // Swap array[j] and array[j+1]
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
     }
 }
